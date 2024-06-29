a1282a611d76bcadc652737d1dacb754c72352ce
Normand <normandlubaton@icloud.com>
2024-06-30 03:16:50 +0800

    added meta data for link sharing

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
     title: "Normand's Portfolio",
-    description: "Web Portfolio of Normand",
+    description: "Web Portfolio of Normand Lubaton",
 };
 
 export default function RootLayout({
@@ -20,6 +20,17 @@ export default function RootLayout({
         <html lang="en" suppressHydrationWarning>
             <head>
                 <link rel="icon" href="/norm.png" sizes="any" />
+                <meta property="og:title" content="Normand's Portfolio" />
+                <meta
+                    property="og:description"
+                    content="Web Portfolio of Normand Lubaton"
+                />
+                <meta property="og:image" content="/norm.png" />
+                <meta
+                    property="og:url"
+                    content="https://www.normandlubaton.dev"
+                />
+                <meta property="og:type" content="website" />
             </head>
             <body className={inter.className}>
                 <ThemeProvider
